47092d3b460243cf79eb84e903d1aa624e4db67b
memoma <gminiala@gmail.com>
2016-07-28 14:12:50 +0800

    add goPre function

--- McCafe_Router.js
+++ McCafe_Router.js
@@ -1,3 +1,6 @@
+/*
+ * @github https://github.com/miniala/McCafe
+ */
 (function (name, definition) {
   if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
     module.exports = definition();
@@ -11,18 +14,21 @@
 })('McCafeRouter', function (McCafeRouter) {
 
   'use strict';
+
 	/*
 	 * @Class McCafeRouter
 	 * @des 前端路由 格式：#/index/:param
 	 */
 	var McCafeRouter = (function () {
-		var state = {
-			routers: [], // [{'path': '/index', 'params': ['a', 'b'], 'component': Function}]
-			router: '', // 当前路由
-			params: {}, // 路由参数
+		var routerState = {
+			routers : [],     // [{'path': '/index', 'params': ['a', 'b'], 'component': Function}]
+			path    : '/',    // 路径
+			params  : {},     // 参数
+			history : [],     // 路由历史
 		}
+
 		/*
-		 * @params routers Array
+		 * @params Array routers 
 		 * @des 创建路由 [{'path': '/index/:a/:b', 'component': Function}]
 		 */
 		function createRouter (routers) {
@@ -40,65 +46,100 @@
 				if (_routersItem.hasOwnProperty('component')) {
 					_router.component = _routersItem.component;
 				}
-				state.routers.push(_router);
+				routerState.routers.push(_router);
 			}
 		}
 
 		/*
+		 * @param String router
 		 * @des 设置当前路由、参数
 		 */
-		function handleRouter () {
-			var _hash = location.hash;
-			var _routers = state.routers;
+		function setRouter (router) {
+			var _router = router || '/';
+			var _routers = routerState.routers;
+			routerState.path = '/'; 
 			for(var i=0,l=_routers.length; i<l; i++) {
 				var _routersItem = _routers[i];
 				var _path = _routersItem.path;
 				var _params = _routersItem.params;
-				var _reg = _path;
+				var _reg = '^' + _path;
 				for (var j=0, ll=_params.length; j<ll; j++) {
 					_reg += '/.{1,}';
 				}
+				_reg += '$';
 				// 是否匹配当前路由
-				if ((new RegExp(_reg)).test(_hash)) {
-					// 设置 router
-					state.router = _path;
+				if ((new RegExp(_reg)).test(_router)) {
+					// 设置 path
+					routerState.path = _path;
 					// 设置 params
-					var _hashPrams = _hash.split(_path)[1].split('/');
+					var _hashPrams = _router.split(_path)[1].split('/');
 					for (var k=0, lll=_routersItem.params.length; k<lll; k++) {
-						state.params[_routersItem.params[k]] = _hashPrams[k+1];
+						routerState.params[_routersItem.params[k]] = _hashPrams[k+1];
 					}
 					break;
 				}
 			}
 		}
+
 		/*
-		 * @param router String
+		 * @param String router
+		 * @des 跳转
 		 */
 		function goTo (router) {
-			location.hash = router;
+			var _router = router || '/';
+			setRouter (_router);
+			routerState.routers.forEach(function (item, index) {
+				if (routerState.path == item.path) {
+					if(item.component && typeof item.component === 'function') {
+						item.component();
+					}
+				}
+			})
+			routerState.history.push(router);
 		}
+
+		/*
+		 * @des 跳转上一个路由
+		 */
+		function goPre () {
+			goTo (routerState.history[routerState.history.length - 1] || '/')
+		}
+
 		/*
 		 * @des 返回当前路由
 		 */
-		function getRouter () {
-			handleRouter ();
-			return state.router;
+
+		function getPath () {
+			return routerState.path;
 		}
+
 		/*
+		 * @param String str
 		 * @des 返回当前路由参数
 		 */
-		function getParams () {
-			return state.params;
+		function getParams (str) {
+			return routerState.params;
 		}
+
+		/*
+		 * @param String param
+		 * @des 返回指定路由参数值
+		 */
+		function getParam (param) {
+			return routerState.params[param];
+		}
+
 		/*
 		 * @des 对外方法
 		 */
 		return {
 			createRouter: createRouter,
-			goTo        : goTo,
-			handleRouter: handleRouter,
-			getRouter   : getRouter,
+			setRouter   : setRouter,
+			getPath     : getPath,
 			getParams   : getParams,
+			getParam    : getParam,
+			goTo        : goTo,
+			goPre       : goPre,
 		}
 	})()
 
